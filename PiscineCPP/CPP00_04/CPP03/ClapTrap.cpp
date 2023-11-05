fa7e21481da143ca3b9e449147f6dbc4026c68e3
Yassine <yassine@yassine.local>
2023-11-05 11:22:57 +0100

    add newfile

--- PiscineCPP/CPP00_04/CPP03/ClapTrap.cpp
+++ PiscineCPP/CPP00_04/CPP03/ClapTrap.cpp
@@ -6,7 +6,17 @@
 /*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 11:03:32 by yassine           #+#    #+#             */
-/*   Updated: 2023/11/05 11:03:33 by yassine          ###   ########.fr       */
+/*   Updated: 2023/11/05 11:17:48 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name) : 
+Name(name) , HitPoints(10), EnergyPoints(10), AttackDamage(0)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
--- PiscineCPP/CPP00_04/CPP03/ClapTrap.hpp
+++ PiscineCPP/CPP00_04/CPP03/ClapTrap.hpp
@@ -6,7 +6,26 @@
 /*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 11:03:42 by yassine           #+#    #+#             */
-/*   Updated: 2023/11/05 11:03:43 by yassine          ###   ########.fr       */
+/*   Updated: 2023/11/05 11:21:41 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string Name;
+	int HitPoints;
+	int EnergyPoints;
+	int AttackDamage;
+public:
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &other);
+	ClapTrap &operator=(const ClapTrap rhs);
+	~ClapTrap();
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
 
