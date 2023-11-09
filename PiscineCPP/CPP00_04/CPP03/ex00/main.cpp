c70c3b548227768810558256c534ea56050999af
Yassine <yassine@yassine.local>
2023-11-09 20:02:26 +0100

    grosse modif

--- PiscineCPP/CPP00_04/CPP03/ex00/main.cpp
+++ PiscineCPP/CPP00_04/CPP03/ex00/main.cpp
@@ -6,17 +6,18 @@
 /*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 15:51:17 by yassine           #+#    #+#             */
-/*   Updated: 2023/11/05 16:09:23 by yassine          ###   ########.fr       */
+/*   Updated: 2023/11/09 20:01:14 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int main(void)
+int main()
 {
-    ClapTrap claptrap("ClapTrap");
-    claptrap.attack("Enemy1"); // Attaque initiale
-    claptrap.beRepaired(1); // Réparation initiale
-    claptrap.takeDamage(1); // Dégâts initiaux
+    ClapTrap clapTrap("ClapTrap");
+    std::string target = "BadTrap";
+    clapTrap.attack(target);
+    clapTrap.takeDamage(3);
+    clapTrap.beRepaired(2);
     return 0;
 }
