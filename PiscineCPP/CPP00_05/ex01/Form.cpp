c6053de692a8445bb76e61fe3636c1ceb2a1d4cc
yasaidi <yasaidi@bess-f1r3s10.clusters.42paris.fr>
2023-11-18 13:21:40 +0100

    push

--- PiscineCPP/CPP00_05/ex01/Form.cpp
+++ PiscineCPP/CPP00_05/ex01/Form.cpp
@@ -6,16 +6,32 @@
 /*   By: yasaidi <yasaidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 10:19:33 by yasaidi           #+#    #+#             */
-/*   Updated: 2023/11/18 10:24:31 by yasaidi          ###   ########.fr       */
+/*   Updated: 2023/11/18 12:43:13 by yasaidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
+#include "Bureaucrat.hpp"
 
-Form::Form() : name("DefaultForm"), sign(false), grade(1), grade_exec(1)
+Form::Form()
+	: _name("DefaultForm"), _signed(false), _grade(1), _grade_exec(1)
 {
+	std::cout << "Constructeur par defaut\n";
+}
+
+Form::Form(std::string name, int grade, int grade_exec)
+	: _name(name), _grade(grade), _grade_exec(grade_exec)
+{
+	std::cout << "Constructeur avec des attribut\n";
+}
+
+Form::Form(const Form &other)
+	: _name(other._name), _grade(other._grade), _grade_exec(other._grade_exec)
+{
+	std::cout << "Constructeur de copie de Form.\n";
 }
 
 Form::~Form()
 {
+	std::cout << "Destructeur de Form.\n";
 }
--- PiscineCPP/CPP00_05/ex01/Form.hpp
+++ PiscineCPP/CPP00_05/ex01/Form.hpp
@@ -6,25 +6,26 @@
 /*   By: yasaidi <yasaidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 10:18:26 by yasaidi           #+#    #+#             */
-/*   Updated: 2023/11/18 11:55:07 by yasaidi          ###   ########.fr       */
+/*   Updated: 2023/11/18 12:42:46 by yasaidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include "Bureaucrat.hpp"
 #include <iostream>
 
 class Form
 {
-  private:
-	std::string const name;
-	bool sign;
-	int const grade;
-	int const grade_exec;
-
   public:
 	Form();
+	Form(std::string name, int grade, int grade_exec);
+	Form(const Form &other);
+	Form &operator=(const Form &rhs);
 	~Form();
-};
 
+  private:
+	std::string const _name;
+	bool _signed;
+	int const _grade;
+	int const _grade_exec;
+};
