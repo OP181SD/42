2cb1061d53ccf84e77afcf6476dd46ba5b7444a2
Yassine <yassine@yassine.local>
2023-11-26 14:09:42 +0100

    finish

--- PiscineCPP/CPP05_09/CPP05/ex01/main.cpp
+++ PiscineCPP/CPP05_09/CPP05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 10:27:15 by yasaidi           #+#    #+#             */
-/*   Updated: 2023/11/26 09:10:34 by yassine          ###   ########.fr       */
+/*   Updated: 2023/11/26 14:00:18 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 int main ()
 {
     Bureaucrat Chloé("Chloé", 24);
-    Form Form1("Form1", 24, 24); 
-    Form Form2("Form2", 12, 13); 
+    Form Formulaire1("Formulard 1", 24, 24); 
+    Form Formulaire2("Formulard 2", 13, 13); 
 
     try {
         std::cout << "Trying to create Form3...\n";
-        Form Form3("Form3", 0, 151); 
+        Form Formulaire3("Formulard 3 ", 0, 151); 
     }
     catch (const std::exception& e) {
         std::cout << "Couldn't create Form3: " << e.what() << std::endl;
     }
 
     std::cout << "\nSigning Form1...\n";
-    Chloé.signForm(Form1); 
+    Chloé.signForm(Formulaire1); 
     std::cout << "Signing Form2...\n";
-    Chloé.signForm(Form2);
+    Chloé.signForm(Formulaire2);
     return 0;
 }
