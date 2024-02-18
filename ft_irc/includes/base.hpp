0717f69323ba04d06ba00fcf8fcf3d0f85589e80
Yassine <yassine@yassine.local>
2024-02-18 12:51:53 +0100

    Begin . IrC

---
+++ ft_irc/includes/base.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 09:50:24 by yassine           #+#    #+#             */
+/*   Updated: 2024/02/18 09:50:54 by yassine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
