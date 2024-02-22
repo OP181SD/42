acaa131eb9508662b67600d520dc0d18e893a144
Yassine <yassine@yassine.local>
2024-02-22 13:08:12 +0100

    finih maybe

--- PiscineCPP/CPP05_09/CPP09/ex02/main.cpp
+++ PiscineCPP/CPP05_09/CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 11:34:21 by yasaidi           #+#    #+#             */
-/*   Updated: 2024/02/16 11:23:48 by yassine          ###   ########.fr       */
+/*   Updated: 2024/02/22 13:07:41 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,6 @@ void printContainer(const Container& container)
     std::cout << std::endl;
 }
 
-
 void	Conversion(int argc, char *argv[], std::vector<int> &vector)
 {
 	long	string_to_long;
@@ -104,36 +103,66 @@ void	Conversion(int argc, char *argv[], std::vector<int> &vector)
 	}
 }
 
+// Insertion sort
 template<typename T>
-void insertionSort(T begin, T end) 
+void InsertionSort(T& container) 
 {
-    for (T i = begin + 1; i != end; ++i) 
+    for (std::size_t i = 1; i < container.size(); ++i) 
 	{
-        typename T::value_type key = *i;
-        T j = i;
-        while (j != begin && *(j - 1) > key) 
+        typename T::value_type key = container[i];
+        std::size_t j = i;
+        while (j > 0 && container[j - 1] > key) 
 		{
-            *j = *(j - 1);
+            container[j] = container[j - 1];
             --j;
         }
-        *j = key;
+        container[j] = key;
     }
 }
 
-template<typename Iterator>
-void MergeInsertSort(Iterator begin, Iterator end) 
+template<typename T>
+void custom_swap(T& a, T& b) 
 {
-    if (std::distance(begin, end) <= 1)
-        return;
-    if (std::distance(begin, end) < 16) 
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+// Ford-Johnson sort
+template<typename Iterator>
+void FordJohnsonSort(Iterator begin, Iterator end) 
+{
+    int n = std::distance(begin, end);
+    
+    std::vector<typename std::iterator_traits<Iterator>::value_type> X(begin, end);
+    
+    for (int i = 0; i + 1 < n; i += 2) 
 	{
-        insertionSort(begin, end);
-        return;
+    	typename std::iterator_traits<Iterator>::value_type& first = X[i];
+    	typename std::iterator_traits<Iterator>::value_type& second = X[i + 1];
+    	if (second < first)
+        	custom_swap(first, second);
+	}
+
+    std::vector<typename std::iterator_traits<Iterator>::value_type> A_cont, B_cont;
+    for (int i = 0; i < n; i++) 
+	{
+        if (i % 2 == 0) 
+            B_cont.push_back(X[i]);
+		else 
+            A_cont.push_back(X[i]);
     }
-    Iterator middle = begin + std::distance(begin, end) / 2;
-    MergeInsertSort(begin, middle);
-    MergeInsertSort(middle, end);
-    std::inplace_merge(begin, middle, end);
+	
+    InsertionSort(A_cont);
+    while (A_cont.size() != X.size()) 
+	{
+        A_cont.push_back(B_cont.back());
+        B_cont.pop_back();
+        InsertionSort(A_cont);
+    }
+	
+	for (int i = 0; i < n; i++)
+		*(begin + i) = X[i];
 }
 
 int main(int argc, char *argv[]) 
@@ -146,18 +175,19 @@ int main(int argc, char *argv[])
     std::cout << "Before: ";
     printContainer(vec);
     clock_t start = clock();
-	MergeInsertSort(vec.begin(), vec.end());
+	FordJohnsonSort(vec.begin(), vec.end());
     clock_t end = clock();
     std::cout << "After: ";
     printContainer(vec);
     double elapsed_time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000;
-    std::cout << "Time to process a range of " << vec.size() << " std::vector " << elapsed_time << " us" << std::endl;
+      std::cout << "Time to process a range of " << vec.size() << " std::vector " << std::fixed << std::setprecision(5) << elapsed_time << " us" << std::endl;
     std::deque<int> dq(vec.begin(), vec.end());
     start = clock();
-	MergeInsertSort(dq.begin(), dq.end());
+	FordJohnsonSort(dq.begin(), dq.end());
     end = clock();
     elapsed_time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000;
-    std::cout << "Time to process a range of " << dq.size() << " std::deqeu " << elapsed_time << " us" << std::endl;
-
+    std::cout << "Time to process a range of " << dq.size() << " std::deque " << std::fixed << std::setprecision(5) << elapsed_time << " us" << std::endl;
     return 0;
 }
+
+
